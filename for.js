ddb46e367422bfa3348981e2dcbb467f018b4b2f
muhammed sabith <sabithhadi67@gmail.com>
2025-07-17 12:57:06 +0530

    starting of loop

---
+++ for.js
@@ -0,0 +1,25 @@
+// for (var i = 1; i <= 5; i++) {
+//     console.log('*'.repeat(i));
+// }
+
+
+// for (var i = 1; i <= 4; i++) {          
+//     var stars = "";                    
+
+//     for (var j = 1; j <= i; j++) {      
+//         stars += "*";                   
+//     }
+
+
+//     console.log(stars);                 
+// }
+
+
+for (i = 0; i <= 2;i++ ) {
+    var star = "";
+
+    for (j = 0; j <= 4;j++ ) {
+        star += "*";
+    }
+    console.log(star);
+}
